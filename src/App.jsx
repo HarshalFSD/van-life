ca6488e65fac911b46e1cd5d9bb05b9d70c330c0
Harshal Surwase <hsurwase@gmail.com>
2024-11-24 15:33:39 +0530

    changes made for original file location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./page/Home.jsx";
 import About from "./page/About.jsx";
+import Vans from "./page/Van.jsx";
+import VanDetail from "./page/VanDetail.jsx";
+import "./server.js";
 import "./App.css";
 
 const App = () => {
@@ -12,11 +15,14 @@ const App = () => {
         </Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
